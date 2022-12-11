25bc34517b80a0687ab99e7174d9c2245108dd7e
Matthew Harvey <matthewtlharvey@gmail.com>
2022-12-11 11:52:50 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Spoura.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
